a081677f5d83c9fd738bb3423206e466b229f2ee
mackwick <mackenziewicker@gmail.com>
2023-11-12 13:31:33 -0500

    landscaper: user can hire a team

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const tools = [
     {name: "Teeth", generates: 1, cost: 0}, //unlimited use
     {name: "Rusty scisors", generates: 5, cost: 5}, //purchase once, unlimited use
     {name: "Old-timey push mower", generates: 50, cost: 25}, //purchase once, unlimited use
-    {name: "Fancy battery mower", generates: "unk", cost: 250} //purchase once
+    {name: "Fancy battery mower", generates: 100, cost: 250}, //purchase once, unlimited use
+    {name: "Starving students", generates: "unk", cost: 500}, //purchase once
 ]
 
 const player = {
