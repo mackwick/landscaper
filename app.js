edbcf525398488b83fc5509ec43bcda61d847304
mackwick <mackenziewicker@gmail.com>
2023-11-12 13:23:33 -0500

    Landscaper: user can buy scissors

--- app.js
+++ app.js
@@ -5,4 +5,29 @@
 
 const tools = [
     {name: "Teeth", generates: 1, cost: 0}
+    {name: "Rusty scisors", generates: "unknown", cost: 5}
 ]
+
+const player = {
+    money: 0,
+    tool: 0
+}
+
+function mowLawn() {
+    const tool = tools[player.tool];
+    alert(`You mowed a lawn with ${tool.name} and made ${tool.generates} dollars!`);
+    player.money += tool.generates
+}
+
+function buyTool() {
+    if (player.tool + 1 < tools.length) {
+        const newTool = tools[player.tool + 1];
+        if (newTool.cost <= player.money) {
+            alert(`You have purchased a ${newTool.name} and spent ${newTool.cost}.`);
+            player.tool = newTool;
+            player.money -= tool.cost
+        } else {
+            alert(`There are no new tools available to buy.`)
+        }
+    }
+}
